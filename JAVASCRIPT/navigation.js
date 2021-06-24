2cce88e63b5de7c64d80bf0978d966159efa7010
Szymandos <szamanburito@gmail.com>
2021-06-25 00:57:05 +0200

    Clear page

--- JAVASCRIPT/navigation.js
+++ JAVASCRIPT/navigation.js
@@ -1,9 +1,18 @@
+// links //
 var home = document.getElementById('containerHOME');
 var about = document.getElementById('containerABOUT');
 var music = document.getElementById('containerMUSIC');
 var websites = document.getElementById('containerWEBSITES');
 var projects = document.getElementById('containerPROJECTS');
 var contact = document.getElementById('containerCONTACT');
+
+// buttons //
+var bhome = document.getElementById('btnHOME');
+var babout = document.getElementById('btnABOUT');
+var bmusic = document.getElementById('btnMUSIC');
+var bwebsites = document.getElementById('btnWEBSITES');
+var bprojects = document.getElementById('btnPROJECTS');
+var bcontact = document.getElementById('btnCONTACT');
 
 // display : none //
 function none() {
@@ -18,7 +27,7 @@ function none() {
 none();
 
 // ABOUT ME //
-document.getElementById('btnABOUT').addEventListener('click', function() {
+babout.addEventListener('click', function() {
         if (about.style.display == "none") {
             //
             about.style.display = "block";
@@ -30,40 +39,43 @@ document.getElementById('btnABOUT').addEventListener('click', function() {
         }
     })
     // MUSIC //
-document.getElementById('btnMUSIC').addEventListener('click', function() {
-    if (music.style.display == "none") {
-        //
-        about.style.display = "none";
-        music.style.display = "block";
-        websites.style.display = "none";
-        projects.style.display = "none";
-        contact.style.display = "none";
-        home.style.display = "none";
-    }
-})
-document.getElementById('btnWEBSITES').addEventListener('click', function() {
-    if (websites.style.display == "none") {
-        //
-        about.style.display = "none";
-        music.style.display = "none";
-        websites.style.display = "block";
-        projects.style.display = "none";
-        contact.style.display = "none";
-        home.style.display = "none";
-    }
-})
-document.getElementById('btnPROJECTS').addEventListener('click', function() {
-    if (projects.style.display == "none") {
-        //
-        about.style.display = "none";
-        music.style.display = "none";
-        websites.style.display = "none";
-        projects.style.display = "block";
-        contact.style.display = "none";
-        home.style.display = "none";
-    }
-})
-document.getElementById('btnCONTACT').addEventListener('click', function() {
+bmusic.addEventListener('click', function() {
+        if (music.style.display == "none") {
+            //
+            about.style.display = "none";
+            music.style.display = "block";
+            websites.style.display = "none";
+            projects.style.display = "none";
+            contact.style.display = "none";
+            home.style.display = "none";
+        }
+    })
+    // WEBSITES //
+bwebsites.addEventListener('click', function() {
+        if (websites.style.display == "none") {
+            //
+            about.style.display = "none";
+            music.style.display = "none";
+            websites.style.display = "block";
+            projects.style.display = "none";
+            contact.style.display = "none";
+            home.style.display = "none";
+        }
+    })
+    // PROJECTS //
+bprojects.addEventListener('click', function() {
+        if (projects.style.display == "none") {
+            //
+            about.style.display = "none";
+            music.style.display = "none";
+            websites.style.display = "none";
+            projects.style.display = "block";
+            contact.style.display = "none";
+            home.style.display = "none";
+        }
+    })
+    // CONTACT //
+bcontact.addEventListener('click', function() {
     if (contact.style.display == "none") {
         //
         about.style.display = "none";
@@ -75,7 +87,7 @@ document.getElementById('btnCONTACT').addEventListener('click', function() {
     }
 })
 
-
-document.getElementById('btnHOME').addEventListener('click', function() {
+// HOME //
+bhome.addEventListener('click', function() {
     none();
 })
