b3b454c9d77b95686bb33bfa42bc352bd0d7fc72
Jakub Melzacki <melzacki.jakub@gmail.com>
2021-09-15 10:51:26 +0200

    hot fixed backed

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require("path")
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.use(express.static(__dirname))
 
@@ -9,6 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname+"/static/HTML/index.html", )
 });
 
-app.listen(port,() => {
-    console.log("run on port 3000 on heroku")
+app.listen(process.env.PORT || 5000,() => {
+    console.log("run on heroku")
 });
