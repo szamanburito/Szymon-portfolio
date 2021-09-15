ad5d0881b6eea7a2e549cb304a7261e7d2b8067a
Jakub Melzacki <melzacki.jakub@gmail.com>
2021-09-15 10:20:02 +0200

    Added backend

---
+++ app.js
@@ -0,0 +1,12 @@
+const path = require("path")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+"/static/HTML/index.html", )
+});
+
+app.listen(port);
